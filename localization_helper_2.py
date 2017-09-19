45ca4ec6c2bff0d5ae39889a071a527ff9d8f5e0
jasonlingkkday <jason.ling@kkday.com>
2017-09-19 19:07:05 +0800

    added sync code and other analyze code

--- localization_helper_2.py
+++ localization_helper_2.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 
+# note:
+# need pip2 install unicodecsv
+
 import os
 import re
 import io
 import string
+import json
 import google_spreadsheet as sheet_api
 import pseudo_localization as pseudo
 
@@ -29,15 +33,33 @@ def is_mostly_ascii(s):
 def col_letter_to_index(letter):
     return string.ascii_uppercase.index(letter.upper())
 
-def print_entries(entries):
+def print_entries(entries, limit=0):
     for index, entry in enumerate(entries):
         print(u"{0}. {1}".format(index+1, entry.key))
         for lang, value in entry.values.iteritems():
             print(u"[{0}]: {1}".format(lang, value))
+        if limit and index + 1 >= limit:
+            break 
 
 def print_entries_key_only(entries):
     for index, entry in enumerate(entries):
         print(u"{0}. {1}".format(index+1, entry.key))
+
+def print_mappings(mappings, limit=0, print_key_only=False):
+    for index, mapping in enumerate(mappings):
+        print(u'key: {0}'.format(mapping.key))
+        print(' - spreadsheet entry:')
+        if print_key_only:
+            print_entries_key_only(mapping.spreadsheet_entries)
+        else:
+            print_entries(mapping.spreadsheet_entries)
+        print(' - strings entry:')
+        if print_key_only:
+            print_entries_key_only(mapping.strings_entries)
+        else:
+            print_entries(mapping.strings_entries)
+        if limit and index + 1 >= limit:
+            break
 
 # ---------- manager / model ----------
 
@@ -63,7 +85,6 @@ class SpreadSheetManager(object):
         self.sheet_start_col = None
         self.sheet_last_col = None
         self.language_cols = None
-        self.key_col = None
     
     @classmethod
     def default_manager(cls):
@@ -74,33 +95,33 @@ class SpreadSheetManager(object):
         manager.sheet_last_row = 800
         manager.sheet_start_col = 'A'
         manager.sheet_last_col = 'R'
-        manager.language_cols = {LANG_EN:'K', 
-                                 LANG_TW:'J',
-                                 LANG_HK:'L',
-                                 LANG_CN:'M',
-                                 LANG_JP:'N',
-                                 LANG_KR:'O',
-                                 LANG_THAI:'P'}
+        manager.language_cols = {LANG_EN:'L', 
+                                 LANG_TW:'K',
+                                 LANG_HK:'M',
+                                 LANG_CN:'N',
+                                 LANG_JP:'O',
+                                 LANG_KR:'P',
+                                 LANG_THAI:'Q'}
         # convert cols from letter to index
         for key, val in manager.language_cols.iteritems():
             manager.language_cols[key] = col_letter_to_index(val)
-        manager.key_col = manager.language_cols[LANG_TW]
         return manager
     
-    def load_spreadsheet_entries(self):
+    def load_spreadsheet_entries(self, lang_for_key=LANG_TW):
         service = sheet_api.deal_with_auth_and_prepare_api_service()
         range_name = u'{0}!{1}{2}:{3}{4}'.format(self.sheet_name,
                                                  self.sheet_start_col, self.sheet_start_row,
                                                  self.sheet_last_col, self.sheet_last_row)
         values = sheet_api.read_spreadsheet_values(service, self.spreadsheet_id, range_name)
         entries = []
+        key_col = self.language_cols[lang_for_key]
         if not values:
             print('No data found from reading google localization spreadsheet'
                   '\nSpreadsheet id: {0} sheet name: {1}'.format(self.spreadsheet_id, self.sheet_name))
             return entries
         for row in values:
             entry = SpreadSheetEntry()
-            entry.key = row[self.key_col]
+            entry.key = row[key_col]
             for lang, col in self.language_cols.iteritems():
                 value = row[col].strip('\n') if len(row) > col else ""
                 entry.values[lang] = value
@@ -118,6 +139,7 @@ class LocalizationStringsManager(object):
         self.app_dir = None
         self.languages = None
         self.base_lang = None
+        self.entry_pattern = re.compile(r'^(?!\/\/).*"(.*?[^\\])"[ ]*=[ ]*"(.*?[^\\])"\s*(;?)(.*)')
 
     @classmethod
     def default_manager(cls):
@@ -132,27 +154,35 @@ class LocalizationStringsManager(object):
                              LANG_THAI:pseudo.THAI_DIR}
         manager.base_lang = LANG_EN
         return manager
-    
+
     def load_strings_entries(self):
         entries = dict()
-        p = re.compile(r'^(?!\/\/).*"(.*?[^\\])"[ ]*=[ ]*"(.*?[^\\])(".*)')
         for lang, lang_dir in self.languages.iteritems():
-            dir_path = os.path.join(self.app_dir, lang_dir)
-            strings_file_path = os.path.join(dir_path, 'Localizable.strings')
-            # open and scan the file
-            with io.open(strings_file_path, encoding='utf8') as reader:
-                for line in reader:
-                    matched = p.match(line)
-                    if matched:
-                        key = matched.group(1)
-                        val = matched.group(2)
-                        entry = entries.get(key)
-                        if not entry:
-                            entry = StringsEntry()
-                            entry.key = key
-                            entries[key] = entry
-                        entry.values[lang] = val
+            for line in self.read_localization_strings_file(lang, lang_dir):
+                matched = self.entry_pattern.match(line)
+                if matched:
+                    key = matched.group(1)
+                    val = matched.group(2)
+                    entry = entries.get(key)
+                    if not entry:
+                        entry = StringsEntry()
+                        entry.key = key
+                        entries[key] = entry
+                    entry.values[lang] = val
         return entries.values()
+    
+    def read_localization_strings_file(self, lang, lang_dir):
+        dir_path = os.path.join(self.app_dir, lang_dir)
+        strings_file_path = os.path.join(dir_path, 'Localizable.strings')
+        with io.open(strings_file_path, encoding='utf8') as reader:
+            for line in reader:
+                yield line
+
+class EntryMapping(object):
+    def __init__(self):
+        self.key = ''
+        self.spreadsheet_entries = list()
+        self.strings_entries = list()
 
 # ---------- testing ----------
 
@@ -192,13 +222,160 @@ def find_not_translated_entries():
             suspect.append(entry)
     print_entries_key_only(suspect)
 
+def verify_localization_strings_syntax():
+    print("---- check localization strings syntax integrity ----")
+    manager = LocalizationStringsManager.default_manager()
+    for lang, lang_dir in manager.languages.iteritems():
+        print(" checking {0} localizable strings".format(lang))
+        line_number = 1
+        file_good = True
+        for line in manager.read_localization_strings_file(lang, lang_dir):
+            matched = manager.entry_pattern.match(line)
+            if matched and matched.group(3) != ";":
+                print (u'  {0}. {1}'.format(line_number, line))
+                file_good = False
+            line_number += 1
+        if file_good:
+            print(' - all good')
+
+def compare_localizable_strings_with_spreadsheet():
+    print("---- compare localization data from spreadsheet and app ----")
+    key_lang = LANG_TW
+    # load spreadsheet entry
+    spreadsheet_manager = SpreadSheetManager.default_manager()
+    spreadsheet_entries = spreadsheet_manager.load_spreadsheet_entries(key_lang)
+    # load localizable.strings entry
+    strings_manager = LocalizationStringsManager.default_manager()
+    strings_entries = strings_manager.load_strings_entries()
+    # map and link up the entry
+    mappings = dict()
+    # use value of english from both set of entries to build mapping
+    #  pass one (spreadsheet)
+    for entry in spreadsheet_entries:
+        key = entry.values[key_lang]
+        mapping = mappings.get(key)
+        if not mapping:
+            mapping = EntryMapping()
+            mapping.key = key
+            mappings[key] = mapping
+        mapping.spreadsheet_entries.append(entry)
+    #  pass two (strings)
+    for entry in strings_entries:
+        key = entry.values[key_lang]
+        mapping = mappings.get(key)
+        if not mapping:
+            mapping = EntryMapping()
+            mapping.key = key
+            mappings[key] = mapping
+        mapping.strings_entries.append(entry)
+    # analyze the difference
+    both = []
+    spreadsheet_only = []
+    strings_only = []
+    for key, mapping in mappings.iteritems():
+        if len(mapping.spreadsheet_entries) > 0 and len(mapping.strings_entries) > 0:
+            both.append(mapping)
+        elif len(mapping.spreadsheet_entries) > 0:
+            spreadsheet_only.append(mapping)
+        else:
+            strings_only.append(mapping)
+    # print the result
+    print(' result:')
+    print(' -- value changed:')
+    index = 1
+    diff_result = dict()
+    for mapping in both:
+        spreadsheet_entry = mapping.spreadsheet_entries[0]
+        strings_entry = mapping.strings_entries[0]
+        difference = dict()
+        for lang, spreadsheet_value in spreadsheet_entry.values.iteritems():
+            strings_value = strings_entry.values.get(lang)
+            if spreadsheet_value != strings_value:
+                difference[lang] = {"sheet": spreadsheet_value, "app": strings_value}
+        if difference:
+            print(u' {0}. ios key: {1}'.format(index, strings_entry.key))
+            for lang, diff in difference.iteritems():
+                print(u'  - [{0}]: {1} --> {2}'.format(lang, diff["app"], diff["sheet"]))
+            diff_result[strings_entry.key] = difference
+            index += 1
+    file_name = 'diff_in_app_sheet.json'
+    print('  write result to json: {0}'.format(file_name))
+    with io.open(file_name, mode='w', encoding='utf8') as writer:
+        to_write = json.dumps(diff_result, ensure_ascii=False)
+        if isinstance(to_write, str):
+            to_write = unicode(to_write, 'UTF-8')
+        writer.write(to_write)
+    print('  .....done!')
+
+    # print('  - both: {0}'.format(len(both)))
+    # print('  - spreadsheet only: {0}'.format(len(spreadsheet_only)))
+    # print_mappings(spreadsheet_only, 10, True)
+    # print('  - strings only: {0}'.format(len(strings_only)))
+    # print_mappings(strings_only, 10, True)
+
+def update_app_localizable_strings_base_on_diff_json():
+    print("---- update localization.strings base on diff json ----")
+    manager = LocalizationStringsManager.default_manager()
+    # read the diff file
+    diff_result = None
+    with open('diff_in_app_sheet.json') as json_data:
+        diff_result = json.load(json_data)
+    if not diff_result:
+        print('no diff_in_app_sheet.json found')
+        return
+    # going though each Localizable.strings files
+    for lang, lang_dir in manager.languages.iteritems():
+        print(" checking {0} localizable strings".format(lang))
+        line_number = 1
+        file_good = True
+        # create a tmp file
+        dir_path = os.path.join(manager.app_dir, lang_dir)
+        strings_file_path = os.path.join(dir_path, 'Localizable.strings')
+        file_to_write = os.path.join(dir_path, 'Localizable.strings.tmp')
+        # remove tmp file
+        if os.path.exists(file_to_write):
+            os.remove(file_to_write)
+        # write to tmp file
+        with io.open(file_to_write, mode='w', encoding='utf8') as writer:
+            print(u"writing to file: {0}".format(file_to_write))
+            for line in manager.read_localization_strings_file(lang, lang_dir):
+                line_to_write = line
+                matched = manager.entry_pattern.match(line)
+                if matched:
+                    key = matched.group(1)
+                    val = matched.group(2)
+                    # bypass any entry with modifier, since updating entry with modifier is dangerous
+                    if not "%" in val:
+                        diff = diff_result.get(key)
+                        if diff:
+                            diff_value = diff.get(lang)
+                            if diff_value:
+                                # sanitize new value
+                                new_value = diff_value["sheet"]
+                                # special condition
+                                if new_value == 'Luggage width is over 21"':
+                                    new_value = 'Luggage width is over 21\"'
+                                # in order to avoid overwrite key (when value and key happens to be the same)
+                                key_part, val_part = line_to_write.split("=")
+                                line_to_write = key_part+"="+val_part.replace(diff_value["app"], new_value)
+                                print(u"    {0} ==> {1}".format(line, line_to_write))
+                writer.write(line_to_write)
+        # replace original file with tmp file
+        os.remove(strings_file_path)
+        os.rename(file_to_write, strings_file_path)
+    # remove diff file
+    os.remove('diff_in_app_sheet.json')
+
 # ---------- main ----------
 
 def main():
     # test_load_spreadsheet_entries()
     # test_load_strings_entries()
     # find_english_only_localization()
-    find_not_translated_entries()
+    # find_not_translated_entries()
+    # verify_localization_strings_syntax()
+    compare_localizable_strings_with_spreadsheet()
+    # update_app_localizable_strings_base_on_diff_json()
 
 if __name__ == '__main__':
     main()
